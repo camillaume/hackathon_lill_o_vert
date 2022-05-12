aa051c00b27b348744eca82a004817c0eeedb5ff
remi59800 <remimenart@gmail.com>
2022-05-12 22:49:47 +0200

    added calculator without style

--- frontend/src/pages/Mobility/index.jsx
+++ frontend/src/pages/Mobility/index.jsx
@@ -1,9 +1,11 @@
 import Vehicule from "../../components/Vehicule/index";
+import Calculator from "../../components/Calculator";
 
 function Mobility() {
   return (
     <div className="mobility">
       <Vehicule />
+      <Calculator />
     </div>
   );
 }
