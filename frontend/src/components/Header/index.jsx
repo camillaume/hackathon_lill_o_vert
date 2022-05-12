cda38a24be508827519ab3a027c360b7d33a911b
camille <camille.arvieu@gmail.com>
2022-05-12 23:13:09 +0200

    header and styling burger

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Burger from "../Burger/index";
+import SHeader from "./style";
+
+export default function Header() {
+  const [burgerOpen, setBurgerOpen] = useState(false);
+  const toggleBurger = () => {
+    setBurgerOpen(!burgerOpen);
+  };
+
+  return (
+    <SHeader>
+      <Link to="/Home" className="logo">
+        <img
+          src="../../../src/assets/pictures/logo-lille-o-vert.svg"
+          alt="logo"
+        />
+      </Link>
+      <Burger burger={burgerOpen} func={toggleBurger} />
+      <div className="navigation">
+        <ul>
+          <Link to="/mobilty">
+            <li>SE DEPLACER CONSCIEMMENT</li>
+          </Link>
+          <Link to="/consume">
+            <li>CONSOMMER RESPONSABLE</li>
+          </Link>
+        </ul>
+      </div>
+    </SHeader>
+  );
+}
