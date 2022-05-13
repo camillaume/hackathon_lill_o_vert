efb13f33415d86c0b29f8f24d11469dc0588ffe1
francois salmon <ashram939@gmail.com>
2022-05-13 10:50:58 +0200

    css footer header consume & sources

--- frontend/src/pages/Sources/index.jsx
+++ frontend/src/pages/Sources/index.jsx
@@ -4,23 +4,23 @@ export default function Sources() {
   return (
     <SSources>
       <div>
-        <h2>SOURCES</h2>
+        <h1>SOURCES</h1>
       </div>
       <p className="baseline">
-        Ci-dessous les les ressources que nous avons utiliser afin de vous
-        proposer un expérience éco-responssable globale sur le territoire de la
-        MEL.
+        Ci-dessous les ressources que nous avons utilisées afin de vous proposer
+        une expérience éco-responsable globale sur le territoire de la MEL.
       </p>
       <ul>
         <li>
-          Une des APIs fournie par l`ADEME permettant d`évaluer son bilant
-          carbonne en fonction d`une distance kilométrique et du moyen de
-          trasport utilisé <a href="https://monconvertisseurco2.fr/">ici</a>
+          Une des APIs fournie par l&apos;ADEME permettant d&apos;évaluer son
+          bilan carbone en fonction d&apos;une distance kilométrique et du moyen
+          de transport utilisé{" "}
+          <a href="https://api.monimpacttransport.fr/">ici</a>
         </li>
         <li>
           {" "}
           Carte fournie par le site Zero Waste Lille, indiquant des lieux pour
-          consommer éco-respossable{" "}
+          consommer éco-resposable{" "}
           <a
             href="https://zerowastelille.org/la-carte-du-vrac-des-hauts-de-france/"
             rel="noreferrer"
@@ -30,7 +30,7 @@ export default function Sources() {
         </li>
         <li>
           {" "}
-          Convertisseur de consomation carbone{" "}
+          Convertisseur de consommation carbone{" "}
           <a href="https://monconvertisseurco2.fr/" rel="noreferrer">
             ici
           </a>
